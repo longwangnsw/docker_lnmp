5f80635aa846df973d80e4f800d5235e411c190c
longwangnsw <42995532+longwangnsw@users.noreply.github.com>
2018-09-06 18:01:27 +1000

    Update index.php

--- app/default/index.php
+++ app/default/index.php
@@ -1,5 +1,8 @@
 <?php
-
-Wellcome to Docker_lnmp maded by David
-
-phpinfo();
+echo "<br>";
+echo "Wellcome to Docker_lnmp maded by David"."<br>";
+echo "<br>";
+echo "Nginx -- php7.1-fpm -- MySQL5.7"."<br>";
+echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|"."<br>";
+echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Redis"."<br>"
+?>
